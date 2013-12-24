beae508b226bf2ac3ab505ccdf56770ea5eabe47
Takayuki Hasegawa <takayuki.hasegawa@gree.net>
2013-12-24 21:32:50 +0900

    initilize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JaxaOpenApi::Application.config.secret_key_base = '1c393020bcfa25caa44bc34f5eaf4832d16a2f820a8f385ebd7f34909142363b3fa6e95d1ef591c31d77e2f81df29629c36a237711268f9a4943d072a5537ca5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JaxaOpenApi::Application.config.session_store :cookie_store, key: '_jaxa_open_api_session'
