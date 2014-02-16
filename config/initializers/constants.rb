e714d89554619b8a570dbc661a55e7995b04e61d
hase1031 <takayuki.hasegawa0311@gmail.com>
2014-02-16 12:57:54 +0900

    add format of temperature and food

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,61 +4,262 @@ PLACE_CHOICES = [
     :place_id => 0,
     :lat => 61.1,
     :lon => 99.2,
-    :place_name => 'Russia - Siberia'
+    :place_name => 'Russia - Siberia',
+    :altitude => "",
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => -14.8 },
+      { :month => 2, :degree => -16.9 },
+      { :month => 3, :degree => -7.5 },
+      { :month => 4, :degree => 2.6 },
+      { :month => 5, :degree => 9.5 },
+      { :month => 6, :degree => 15.5 },
+      { :month => 7, :degree => 18.3 },
+      { :month => 8, :degree => 18.2 },      
+      { :month => 9, :degree => 8.5 },
+      { :month => 10, :degree => 1.8 },
+      { :month => 11, :degree => -2.8 },
+      { :month => 12, :degree => -10.5 }     
+    ]
   },
   {
     :place_id => 1,
     :lat => 23.8,
     :lon => 11.3,
-    :place_name => 'Algeria - The Sahara'
+    :place_name => 'Algeria - The Sahara',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => 20.1 },
+      { :month => 2, :degree => 22.1 },
+      { :month => 3, :degree => 25.4 },
+      { :month => 4, :degree => 30.1 },
+      { :month => 5, :degree => 33.6 },
+      { :month => 6, :degree => 35.4 },
+      { :month => 7, :degree => 35.3 },
+      { :month => 8, :degree => 34.7 },      
+      { :month => 9, :degree => 33.2 },
+      { :month => 10, :degree => 29.5 },
+      { :month => 11, :degree => 24.7 },
+      { :month => 12, :degree => 21.3 }
+    ]
   },
   {
     :place_id => 2,
     :lat => 7.0,
     :lon => -73.0,
-    :place_name => 'Colombia - Piedequesta'
+    :place_name => 'Colombia - Piedequesta',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => 16.4 },
+      { :month => 2, :degree => 16.7 },
+      { :month => 3, :degree => 16.7 },
+      { :month => 4, :degree => 16.0 },
+      { :month => 5, :degree => 16.2 },
+      { :month => 6, :degree => 15.7 },
+      { :month => 7, :degree => 14.9 },
+      { :month => 8, :degree => 15.4 },      
+      { :month => 9, :degree => 15.8 },
+      { :month => 10, :degree => 15.9 },
+      { :month => 11, :degree => 16.0 },
+      { :month => 12, :degree => 16.1 }
+    ]
   },
   {
     :place_id => 3,
     :lat => 39.0,
     :lon => 141.0,
-    :place_name => 'Japan - Iwate'
+    :place_name => 'Japan - Iwate',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => "" },
+      { :month => 2, :degree => "" },
+      { :month => 3, :degree => "" },
+      { :month => 4, :degree => "" },
+      { :month => 5, :degree => "" },
+      { :month => 6, :degree => "" },
+      { :month => 7, :degree => "" },
+      { :month => 8, :degree => "" },      
+      { :month => 9, :degree => "" },
+      { :month => 10, :degree => "" },
+      { :month => 11, :degree => "" },
+      { :month => 12, :degree => "" }
+    ]
   },
   {
     :place_id => 4,
     :lat => 31.0,
     :lon => -100.0,
-    :place_name => 'the U.S. - Texas'
+    :place_name => 'the U.S. - Texas',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => 16.1 },
+      { :month => 2, :degree => 18.5 },
+      { :month => 3, :degree => 21.7 },
+      { :month => 4, :degree => 25.8 },
+      { :month => 5, :degree => 29.2 },
+      { :month => 6, :degree => 32.3 },
+      { :month => 7, :degree => 33.7 },
+      { :month => 8, :degree => 33.6 },      
+      { :month => 9, :degree => 31.3 },
+      { :month => 10, :degree => 27.6 },
+      { :month => 11, :degree => 22.4 },
+      { :month => 12, :degree => 18.2 }
+    ]
   },
   {
     :place_id => 5,
     :lat => 43.7,
     :lon => 39.9,
-    :place_name => 'Russia - Sochi'
+    :place_name => 'Russia - Sochi',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => "" },
+      { :month => 2, :degree => "" },
+      { :month => 3, :degree => "" },
+      { :month => 4, :degree => "" },
+      { :month => 5, :degree => "" },
+      { :month => 6, :degree => "" },
+      { :month => 7, :degree => "" },
+      { :month => 8, :degree => "" },      
+      { :month => 9, :degree => "" },
+      { :month => 10, :degree => "" },
+      { :month => 11, :degree => "" },
+      { :month => 12, :degree => "" }
+    ]
   },
   {
     :place_id => 6,
     :lat => 69.4,
     :lon => 88.4,
-    :place_name => 'Russia - Norilsk'
+    :place_name => 'Russia - Norilsk',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => "" },
+      { :month => 2, :degree => "" },
+      { :month => 3, :degree => "" },
+      { :month => 4, :degree => "" },
+      { :month => 5, :degree => "" },
+      { :month => 6, :degree => "" },
+      { :month => 7, :degree => "" },
+      { :month => 8, :degree => "" },      
+      { :month => 9, :degree => "" },
+      { :month => 10, :degree => "" },
+      { :month => 11, :degree => "" },
+      { :month => 12, :degree => "" }
+    ]
   },
   {
     :place_id => 7,
     :lat => -34.0,
     :lon => 151.3,
-    :place_name => 'Australia - Sydney'
+    :place_name => 'Australia - Sydney',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => "" },
+      { :month => 2, :degree => "" },
+      { :month => 3, :degree => "" },
+      { :month => 4, :degree => "" },
+      { :month => 5, :degree => "" },
+      { :month => 6, :degree => "" },
+      { :month => 7, :degree => "" },
+      { :month => 8, :degree => "" },      
+      { :month => 9, :degree => "" },
+      { :month => 10, :degree => "" },
+      { :month => 11, :degree => "" },
+      { :month => 12, :degree => "" }
+    ]
   },
   {
     :place_id => 8,
     :lat => 51.6,
     :lon => 0.2,
-    :place_name => 'England - London'
+    :place_name => 'England - London',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => 3.6 },
+      { :month => 2, :degree => 4.1 },
+      { :month => 3, :degree => 5.6 },
+      { :month => 4, :degree => 7.9 },
+      { :month => 5, :degree => 11.1 },
+      { :month => 6, :degree => 14.3 },
+      { :month => 7, :degree => 16.1 },
+      { :month => 8, :degree => 15.9 },      
+      { :month => 9, :degree => 13.7 },
+      { :month => 10, :degree => 10.7 },
+      { :month => 11, :degree => 6.4 },
+      { :month => 12, :degree => 4.4 }
+    ]
   },
   {
     :place_id => 9,
     :lat => -1.5,
     :lon => 36.9,
-    :place_name => 'Kenya - Nairobi'
+    :place_name => 'Kenya - Nairobi',
+    :food => [
+      { :season_id => 1, :name => "" },
+      { :season_id => 2, :name => "" },
+      { :season_id => 3, :name => "" },
+      { :season_id => 4, :name => "" }
+    ],
+    :temperature => [
+      { :month => 1, :degree => "" },
+      { :month => 2, :degree => "" },
+      { :month => 3, :degree => "" },
+      { :month => 4, :degree => "" },
+      { :month => 5, :degree => "" },
+      { :month => 6, :degree => "" },
+      { :month => 7, :degree => "" },
+      { :month => 8, :degree => "" },      
+      { :month => 9, :degree => "" },
+      { :month => 10, :degree => "" },
+      { :month => 11, :degree => "" },
+      { :month => 12, :degree => "" }
+    ]
   },
 #  {
 #    :place_id => 4,
